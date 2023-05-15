6e47177eefe83859ac4f391e5f085bb18037b3c4
maruvuina <bloody.alica@gmail.com>
2023-05-15 15:39:42 +0300

    updated dependencies import

--- src/main/java/com/idfinance/lab/cryptocurrency/model/CryptoCurrency.java
+++ src/main/java/com/idfinance/lab/cryptocurrency/model/CryptoCurrency.java
@@ -3,8 +3,6 @@ package com.idfinance.lab.cryptocurrency.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
